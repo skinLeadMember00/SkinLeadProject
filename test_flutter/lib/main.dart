459be651911aa39b37b23d2751c60f00da9f6172
skinLeadMember00 <githubtestacc@gmail.com>
2019-03-25 14:53:35 +0700

    Update main.dart

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -10,9 +10,80 @@ class MyApp extends StatelessWidget {
     final _suggestions = <WordPair>[];
     _suggestions.addAll(generateWordPairs().take(10));
 
+    var list = ListView.builder(
+      itemBuilder: (context, position)
+      {
+        return Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>
+              [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                      const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
+                      child: Text(
+                        _suggestions[position].asPascalCase,
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                      const EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 12.0),
+                      child: Text(
+                        _suggestions[position].asPascalCase,
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        "5m",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              height: 2.0,
+              color: Colors.grey,
+            )
+          ],
+        );
+      },
+      itemCount: _suggestions.length,
+    );
 
 
-    //
+    return new MaterialApp
+      (
+        title: "",
+        home: new Scaffold(appBar: new AppBar(
+            title: new Text("Card example")
+        ),
+
+          body: list,
+          backgroundColor: Colors.white,
+        )
+    );
+  }
 
 }
+
+
+
+
+
+
 
