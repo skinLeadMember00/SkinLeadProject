688132bd84e00d1f74a41965b7d66b26b03d8d03
skinLeadMember00 <githubtestacc@gmail.com>
2019-03-27 04:38:49 +0700

    Revert "Description"
    This reverts commit 3b5c5af9b1c823c4dde7616c24811640af294aca.

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -10,75 +10,102 @@ class MyApp extends StatelessWidget {
     final _suggestions = <WordPair>[];
     _suggestions.addAll(generateWordPairs().take(10));
 
-    var list = ListView.builder(
-      itemBuilder: (context, position)
-      {
-        return Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>
-              [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding:
-                      const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
-                      child: Text(
-                        _suggestions[position].asPascalCase,
-                        style: TextStyle(
-                            fontSize: 22.0, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                      const EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 12.0),
-                      child: Text(
-                        _suggestions[position].asPascalCase,
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text(
-                        "5m",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Divider(
-              height: 2.0,
-              color: Colors.grey,
-            )
-          ],
+    var card = ListView.builder
+      (
+      itemBuilder: (context, i) {
+        return Card
+          (
+          child: new Column(
+            children: <Widget>[
+              new ListTile(
+                leading: new Icon(Icons.photo, color: Colors.blue,size: 40,),
+                title: new Text(_suggestions[i].asPascalCase, style: new TextStyle(fontWeight: FontWeight.w400),),
+                subtitle: new Text("This is subtitle"),
+              ),
+
+              new Divider(color: Colors.blue, indent: 16.0,),
+            ],
+          ),
         );
       },
       itemCount: _suggestions.length,
     );
 
 
-    return new MaterialApp
-      (
+    final sizedBox = new Container(
+      margin: new EdgeInsets.only(left: 20, right: 20),
+      child: new SizedBox(
+        //height: 220.0,
+        child: card,
+
+      ),
+    );
+    final center = new Center(
+      child: sizedBox,
+
+    );
+
+    return new MaterialApp(
         title: "",
+        //      home: new Text("Add Google fonts to Flutter App")
         home: new Scaffold(appBar: new AppBar(
             title: new Text("Card example")
         ),
-
-          body: list,
-          backgroundColor: Colors.white,
+          body: center,
+          backgroundColor: Colors.indigo,
         )
     );
   }
+}
 
+class RandomWordsState extends State<RandomWords>
+{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SkinLead list of products'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions()
+  {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i)
+        {
+          if(i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if(index >= _suggestions.length)
+          {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return  ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget
+{
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
 
 
